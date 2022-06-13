c972dc174382d96dec820fc59e8ad8d758cb0aed
1410310031 <moustymainer@gmail.com>
2022-06-13 11:20:36 +0100

    Updated settings

--- food/settings.py
+++ food/settings.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
